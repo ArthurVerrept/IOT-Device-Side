91b0cb6630cee170caa32cd0d72c01fd2059d42e
ArthurVerrept <Arthur-verrept@hotmail.co.uk>
2019-04-04 16:23:24 +0100

    added final comments and cleaned up code

--- app.js
+++ app.js
@@ -1,62 +1,54 @@
+//requiring necessary npm packages
 var five = require("johnny-five");
 var board = new five.Board();
 const axios = require("axios");
 
 
-
+//once board is on do this 
 board.on("ready", function() {
   // Create a new generic sensor instance for
   // a sensor connected to an analog (ADC) pin
+  // sets the minimum change threshold to 1 to pick up every sound
   var sensor = new five.Sensor({pin: "A0", threshold: 1});
 	
   //sets count of 0
-  //sets pastMin and currentMin value to be equal
   var count = 0;
+  //sets pastMin and currentMin value to be equal
   var pastMin = new Date().getMinutes();
   var currentMin = new Date().getMinutes();
   
-  // When the sensor value changes, log the value
+  //every time the sensor value changes, log the value
   sensor.on("change", function(value) {
 	  //once a change happens, reset current min 
 	  currentMin = new Date().getMinutes();
-	  //check if currentmin is not equal to before the change happened
+	  //check if currentmin is not equal to past min the change happened
+	  //check if seconds are zero in which case send as long as
+	  //count is greater than 50, this stops axios posting 8 times in one second
 	  if (currentMin != pastMin || new Date().getSeconds == 0 && count > 50)
 	  {	
+	    //create d date object
+	    d = new Date();
+	    //post through axios
 	      axios.post('https://enigmatic-taiga-10166.herokuapp.com/addData', {
+	      //send current count, date as object, and each aspect of date time in seperate`
 	      count: count,
-	      fullTime: new Date().toLocaleTimeString(),
+	      dateTime: d,
 	      hour: new Date().getHours(),
 	      minute: new Date().getMinutes(),
 	      second: 0,
 	      day: new Date().getDate(),
 	      month: new Date().getMonth(),
 	      year: new Date().getFullYear()
-	      
 	    });
-	  //if they are not equal a post request must be made sending the total for prev minute
 	  //after post request is made reset count and start new 
 	  count = 1;
 	  pastMin = new Date().getMinutes();
+	  console.log("post successful")
 	  }
+	  //add 1 to count
 	  else
 	  {
 		  count++;
 	  }  
-	//value = (value * 100) - 50000;
-	console.log(count, currentMin, pastMin, new Date().getSeconds());
   });
 });
-
-
-function myDate(){
-
-	var d = new Date().getDate();
-	var m = new Date().getMonth();
-	var y = new Date().getFullYear();
-
-	return date={
-	  day: d,
-	  month: m,
-	  year: y
-	}
-}
